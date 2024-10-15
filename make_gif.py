b08d730c0ec1cca0dfa82574f9d63db22def1589
Gleb Kuznetsov <64061324+Zea-Zee@users.noreply.github.com>
2024-10-15 23:54:07 +0700

    upd readme

--- make_gif.py
+++ make_gif.py
@@ -2,16 +2,16 @@ import imageio
 import os
 from natsort import natsorted  # Импортируем natsorted
 
-output_dir = "MNIST-GAN-EVO_2024-10-15_18-54-29"
+output_dir = "SIN-GAN-EVO_2024-10-15_19-54-00"
 image_files = [f for f in os.listdir(output_dir) if f.endswith('.png')]
 
 # Используем natsorted для сортировки файлов
 image_files = natsorted(image_files)
 
 images = []
-for image_file in image_files:
+for i, image_file in enumerate(image_files):
     image_path = os.path.join(output_dir, image_file)
     image = imageio.imread(image_path)
     images.append(image)
 
-imageio.mimsave('MNIST_training_evolution.gif', images, fps=10)
+imageio.mimsave('new_sinus_training_evolution.gif', images, fps=25)
